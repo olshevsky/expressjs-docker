5ae1ffd31ae495270a8b2a6b098f99ffefa1d70e
dev <artur.olshevsky@gmail.com>
2023-02-03 07:38:05 +0200

    add hot reload

--- www/app.js
+++ www/app.js
@@ -3,13 +3,8 @@ const app = express();
 const router = express.Router();
 const port = 8080;
 
-router.use(function (req, res, next) {
-  console.log('/' + req.method);
-  next();
-});
-
 router.get('/',function(req, res){
-  res.sendFile(__dirname + 'index.html');
+  res.send('<h1>Hello world!</h1>');
 });
 
 app.use(express.static(__dirname));
